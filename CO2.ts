aef48f4de81e5519488a3949e35d2c068e4a7688
araragoo <50019143+araragoo@users.noreply.github.com>
2021-01-08 19:35:29 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -46,19 +46,17 @@ namespace CO2 {
         basic.pause(100)
 
         buffer = serial.readBuffer(9)
-        if (buffer.getNumber(NumberFormat.UInt8LE, 0) == 255 && buffer.getNumber(NumberFormat.UInt8LE, 1) == 134) {
-            let sum = 0
-            for (let index = 0; index <= 7; index++) {
-                sum = sum + buffer.getNumber(NumberFormat.UInt8LE, index)
-            }
-            sum = sum % 256
-            sum = 255 - sum
-            if (sum == buffer.getNumber(NumberFormat.UInt8LE, 8)) {
-                CO2data = buffer.getNumber(NumberFormat.UInt8LE, 2) * 256 + buffer.getNumber(NumberFormat.UInt8LE, 3)
-                // basic.showNumber(value)
-                // lcd.showLine2(convertToText(value))
-            }
-        }
+//        if (buffer.getNumber(NumberFormat.UInt8LE, 0) == 255 && buffer.getNumber(NumberFormat.UInt8LE, 1) == 134) {
+//            let sum = 0
+//            for (let index = 0; index <= 7; index++) {
+//                sum = sum + buffer.getNumber(NumberFormat.UInt8LE, index)
+//            }
+//            sum = sum % 256
+//            sum = 255 - sum
+//            if (sum == buffer.getNumber(NumberFormat.UInt8LE, 8)) {
+//                CO2data = buffer.getNumber(NumberFormat.UInt8LE, 2) * 256 + buffer.getNumber(NumberFormat.UInt8LE, 3)
+//            }
+//        }
         return CO2data
     }
 
