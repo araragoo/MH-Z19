a06666a8b160e448bc0ba406f9dd5e74d1bc9821
araragoo <50019143+araragoo@users.noreply.github.com>
2021-01-08 17:17:29 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -11,68 +11,18 @@ namespace CO2 {
     //% blockId=initalCO2
     //% block="Init CO2"
     export function initCO2 () {
-/*
-        serial.redirect(
-            SerialPin.P13,
-            SerialPin.P14,
-            BaudRate.BaudRate9600
-        )
-
-        buf.setNumber(NumberFormat.UInt8LE, 0, 255)
-        buf.setNumber(NumberFormat.UInt8LE, 1, 1)
-        buf.setNumber(NumberFormat.UInt8LE, 2, 134)
-        buf.setNumber(NumberFormat.UInt8LE, 3, 0)
-        buf.setNumber(NumberFormat.UInt8LE, 4, 0)
-        buf.setNumber(NumberFormat.UInt8LE, 5, 0)
-        buf.setNumber(NumberFormat.UInt8LE, 6, 0)
-        buf.setNumber(NumberFormat.UInt8LE, 7, 0)
-        buf.setNumber(NumberFormat.UInt8LE, 8, 121)
-
-        buf0.setNumber(NumberFormat.UInt8LE, 0, 255)
-        buf0.setNumber(NumberFormat.UInt8LE, 1, 1)
-        buf0.setNumber(NumberFormat.UInt8LE, 2, 135)
-        buf0.setNumber(NumberFormat.UInt8LE, 3, 0)
-        buf0.setNumber(NumberFormat.UInt8LE, 4, 0)
-        buf0.setNumber(NumberFormat.UInt8LE, 5, 0)
-        buf0.setNumber(NumberFormat.UInt8LE, 6, 0)
-        buf0.setNumber(NumberFormat.UInt8LE, 7, 0)
-        buf0.setNumber(NumberFormat.UInt8LE, 8, 120)
-*/
     }
 
     //  subcategory="CO2"
     //% blockId=measureCO2
     //% block="CO2[ppm]"
     export function measuredValue () {
-/*
-        serial.writeBuffer(buf)
-        basic.pause(100)
-
-        let buffer = serial.readBuffer(9)
-        if (buffer.getNumber(NumberFormat.UInt8LE, 0) == 255 && buffer.getNumber(NumberFormat.UInt8LE, 1) == 134) {
-            let sum = 0
-            for (let index = 0; index <= 7; index++) {
-                sum = sum + buffer.getNumber(NumberFormat.UInt8LE, index)
-            }
-            sum = sum % 256
-            sum = 255 - sum
-            if (sum == buffer.getNumber(NumberFormat.UInt8LE, 8)) {
-                CO2data = buffer.getNumber(NumberFormat.UInt8LE, 2) * 256 + buffer.getNumber(NumberFormat.UInt8LE, 3)
-                // basic.showNumber(value)
-                // lcd.showLine2(convertToText(value))
-            }
-        }*/
-        return CO2data
     }
 
     //  subcategory="CO2"
     //% blockId=setOffset
     //% block="Set CO2 value as 400ppm"
     export function setOffset () {
-/*
-        serial.writeBuffer(buf0)
-        basic.pause(100)
-*/
     }
 }
 
