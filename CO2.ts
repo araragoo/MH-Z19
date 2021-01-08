6a04fc59e7ebbd6db76bde8b94150f67421108f6
araragoo <50019143+araragoo@users.noreply.github.com>
2021-01-08 20:27:13 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -66,6 +66,16 @@ namespace CO2 {
     //% block="Set CO2 value as 400ppm"
     export function setOffset () {
 
+        for (let m = 0; index < 20; index++) {
+            basic.showString("" + convertToText(21 - m) + "min")
+            for (let index = 0; index < 12; index++) {
+                basic.pause(5000)
+            }
+        }
+        for (let m = 0; index < 60; index++) {
+            basic.showString("" + convertToText(60 - m) + "s")
+        }
+
         serial.writeBuffer(buf0)
         basic.pause(100)
     }
