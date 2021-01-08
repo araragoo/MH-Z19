f3327ce9ee307a73cf99916950f9c5eb3ce7decb
araragoo <50019143+araragoo@users.noreply.github.com>
2021-01-08 12:44:15 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -60,56 +60,19 @@ namespace CO2 {
                 CO2data = buffer.getNumber(NumberFormat.UInt8LE, 2) * 256 + buffer.getNumber(NumberFormat.UInt8LE, 3)
                 // basic.showNumber(value)
                 // lcd.showLine2(convertToText(value))
-=======
-        buf.setNumber(NumberFormat.UInt8LE, 0, 255)
-        buf.setNumber(NumberFormat.UInt8LE, 1,   1)
-        buf.setNumber(NumberFormat.UInt8LE, 2, 134)
-        buf.setNumber(NumberFormat.UInt8LE, 3,   0)
-        buf.setNumber(NumberFormat.UInt8LE, 4,   0)
-        buf.setNumber(NumberFormat.UInt8LE, 5,   0)
-        buf.setNumber(NumberFormat.UInt8LE, 6,   0)
-        buf.setNumber(NumberFormat.UInt8LE, 7,   0)
-        buf.setNumber(NumberFormat.UInt8LE, 8, 121)
-        serial.writeBuffer(buf)
-        basic.pause(100)
-        let buffer = serial.readBuffer(9)
-        if (buffer.getNumber(NumberFormat.UInt8LE, 0) == 255 && buffer.getNumber(NumberFormat.UInt8LE, 1) == 134) {
-            sum = 0
-            for (let i = 0; i <= 7; i++) {
-                sum = sum + buffer.getNumber(NumberFormat.UInt8LE, i)
->>>>>>> f534faa8a6ee77122853894515e1a879a68daf44
             }
-//            if (buffer.getNumber(NumberFormat.UInt8LE, 8) == sum % 255) {
-                value = buffer.getNumber(NumberFormat.UInt8LE, 2) * 255 + buffer.getNumber(NumberFormat.UInt8LE, 3)
-//            }
         }
-<<<<<<< HEAD
         return CO2data
-=======
-
-        return value
->>>>>>> f534faa8a6ee77122853894515e1a879a68daf44
     }
 
     //  subcategory="CO2"
     //% blockId=setOffset
     //% block="Set CO2 value as 400ppm"
     export function setOffset () {
-<<<<<<< HEAD
+
         serial.writeBuffer(buf0)
         basic.pause(100)
-=======
-        buf.setNumber(NumberFormat.UInt8LE, 0, 255)
-        buf.setNumber(NumberFormat.UInt8LE, 1,   1)
-        buf.setNumber(NumberFormat.UInt8LE, 2, 135)
-        buf.setNumber(NumberFormat.UInt8LE, 3,   0)
-        buf.setNumber(NumberFormat.UInt8LE, 4,   0)
-        buf.setNumber(NumberFormat.UInt8LE, 5,   0)
-        buf.setNumber(NumberFormat.UInt8LE, 6,   0)
-        buf.setNumber(NumberFormat.UInt8LE, 7,   0)
-        buf.setNumber(NumberFormat.UInt8LE, 8, 120)
-        serial.writeBuffer(buf)
->>>>>>> f534faa8a6ee77122853894515e1a879a68daf44
+
     }
 }
 
