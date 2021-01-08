fc783712cc19ca25e48ac91482ee19669fcbb3ee
araragoo <50019143+araragoo@users.noreply.github.com>
2021-01-08 12:35:08 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -5,30 +5,70 @@ namespace CO2 {
 
     let CO2data = 400
 
+    let buf  = control.createBuffer(9)
+    let buf0 = control.createBuffer(9)
+
+    //  subcategory="CO2"
+    //% blockId=initalCO2
+    //% block="Init CO2"
+    export function initCO2 () {
+        serial.redirect(
+            SerialPin.P13,
+            SerialPin.P14,
+            BaudRate.BaudRate9600
+        )
+
+        buf.setNumber(NumberFormat.UInt8LE, 0, 255)
+        buf.setNumber(NumberFormat.UInt8LE, 1, 1)
+        buf.setNumber(NumberFormat.UInt8LE, 2, 134)
+        buf.setNumber(NumberFormat.UInt8LE, 3, 0)
+        buf.setNumber(NumberFormat.UInt8LE, 4, 0)
+        buf.setNumber(NumberFormat.UInt8LE, 5, 0)
+        buf.setNumber(NumberFormat.UInt8LE, 6, 0)
+        buf.setNumber(NumberFormat.UInt8LE, 7, 0)
+        buf.setNumber(NumberFormat.UInt8LE, 8, 121)
+
+        buf0.setNumber(NumberFormat.UInt8LE, 0, 255)
+        buf0.setNumber(NumberFormat.UInt8LE, 1, 1)
+        buf0.setNumber(NumberFormat.UInt8LE, 2, 135)
+        buf0.setNumber(NumberFormat.UInt8LE, 3, 0)
+        buf0.setNumber(NumberFormat.UInt8LE, 4, 0)
+        buf0.setNumber(NumberFormat.UInt8LE, 5, 0)
+        buf0.setNumber(NumberFormat.UInt8LE, 6, 0)
+        buf0.setNumber(NumberFormat.UInt8LE, 7, 0)
+        buf0.setNumber(NumberFormat.UInt8LE, 8, 120)
+    }
+
     //  subcategory="CO2"
     //% blockId=measureCO2
     //% block="CO2[ppm]"
     export function measuredValue () {
-        serial.writeString("" + ([255, 1, 134, 0, 0, 0, 0, 0, 121]))
+        serial.writeBuffer(buf)
         basic.pause(100)
-        data = serial.readBuffer(9)
-        if (data[0] == 255 && data[1] == 134) {
-            sum = 0
+
+        let buffer = serial.readBuffer(9)
+        if (buffer.getNumber(NumberFormat.UInt8LE, 0) == 255 && buffer.getNumber(NumberFormat.UInt8LE, 1) == 134) {
+            let sum = 0
             for (let index = 0; index <= 7; index++) {
-                sum = sum + data[index]
+                sum = sum + buffer.getNumber(NumberFormat.UInt8LE, index)
             }
-            if (data[8] == sum % 255) {
-                value = data[2] * 255 + data[3]
+            sum = sum % 256
+            sum = 255 - sum
+            if (sum == buffer.getNumber(NumberFormat.UInt8LE, 8)) {
+                CO2data = buffer.getNumber(NumberFormat.UInt8LE, 2) * 256 + buffer.getNumber(NumberFormat.UInt8LE, 3)
+                // basic.showNumber(value)
+                // lcd.showLine2(convertToText(value))
             }
         }
-        return value
+        return CO2data
     }
 
     //  subcategory="CO2"
     //% blockId=setOffset
     //% block="Set CO2 value as 400ppm"
     export function setOffset () {
-        serial.writeString("" + ([255, 1, 135, 0, 0, 0, 0, 0, 120]))
+        serial.writeBuffer(buf0)
+        basic.pause(100)
     }
 }
 
