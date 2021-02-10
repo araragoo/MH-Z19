6426f447bd15098af671cd2dd3d91ee0b757c94b
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2021-02-10 10:16:15 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -83,6 +83,160 @@ namespace CO2 {
 
 
 
+
+
+
+
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = value;
+        pins.i2cWriteBuffer(addr, buf);
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function bitMask(reg: number, mas: numberk, thing: number){
+        let originalContents = i2cread(MAX30105_ADDRESS, reg);
+        originalContents = originalContents & mask;
+      i2cwrite(MAX30105_ADDRESS, reg, originalContents | thing);
+    }
+
+
+
+
+    function softReset() {
+        bitMask(MAX30105_MODECONFIG, MAX30105_RESET_MASK, MAX30105_RESET);
+        let startTime = input.running_time_micros();
+        basic.pause(100);
+    }
+
+    function setFIFOAverage(numberOfSamples: number) {
+        bitMask(MAX30105_FIFOCONFIG, MAX30105_SAMPLEAVG_MASK, numberOfSamples);
+    }
+
+    function MAX30105::setFIFOAlmostFull(numberOfSamples: number) {
+        bitMask(MAX30105_FIFOCONFIG, MAX30105_A_FULL_MASK, numberOfSamples);
+    }
+
+    function enableFIFORollover() {
+        bitMask(MAX30105_FIFOCONFIG, MAX30105_ROLLOVER_MASK, MAX30105_ROLLOVER_ENABLE);
+    }
+
+    function setLEDMode(mode: number) {
+        activeDiodes = mode - 1;
+        bitMask(MAX30105_MODECONFIG, MAX30105_MODE_MASK, mode);
+    }
+
+    function setADCRange(adcRange: number) {
+        bitMask(MAX30105_PARTICLECONFIG, MAX30105_ADCRANGE_MASK, adcRange);
+    }
+
+    function setSampleRate(sampleRate: number) {
+        bitMask(MAX30105_PARTICLECONFIG, MAX30105_SAMPLERATE_MASK, sampleRate);
+    }
+
+    function setPulseWidth(pulseWidth: number) {
+        bitMask(MAX30105_PARTICLECONFIG, MAX30105_PULSEWIDTH_MASK, pulseWidth);
+    }
+
+    function setPulseAmplitudeRed(amplitude: number) {
+        i2cwrite(MAX30105_ADDRESS, MAX30105_LED1_PULSEAMP, amplitude);
+    }
+
+    function setPulseAmplitudeIR(amplitude: number) {
+        i2cwrite(MAX30105_ADDRESS, MAX30105_LED2_PULSEAMP, amplitude);
+    }
+
+    function enableSlot(slotNumber: number, device: number) {
+
+      switch (slotNumber) {
+        case (1):
+          bitMask(MAX30105_MULTILEDCONFIG1, MAX30105_SLOT1_MASK, device);
+          break;
+        case (2):
+          bitMask(MAX30105_MULTILEDCONFIG1, MAX30105_SLOT2_MASK, device << 4);
+          break;
+        case (3):
+          bitMask(MAX30105_MULTILEDCONFIG2, MAX30105_SLOT3_MASK, device);
+          break;
+        case (4):
+          bitMask(MAX30105_MULTILEDCONFIG2, MAX30105_SLOT4_MASK, device << 4);
+          break;
+        default:
+          break;
+      }
+    }
+
+    function clearFIFO() {
+      writeRegister8(MAX30105_ADDRESS, MAX30105_FIFOWRITEPTR, 0);
+      writeRegister8(MAX30105_ADDRESS, MAX30105_FIFOOVERFLOW, 0);
+      writeRegister8(MAX30105_ADDRESS, MAX30105_FIFOREADPTR, 0);
+    }
+
+    function particle_setup(powerLevel: number, sampleAverage: number, ledMode: number, sampleRate: number, pulseWidth: number, adcRange: number) {
+        softReset();
+        if (sampleAverage == 1) setFIFOAverage(MAX30105_SAMPLEAVG_1); //No averaging per FIFO record
+        else if (sampleAverage == 2) setFIFOAverage(MAX30105_SAMPLEAVG_2);
+        else if (sampleAverage == 4) setFIFOAverage(MAX30105_SAMPLEAVG_4);
+        else if (sampleAverage == 8) setFIFOAverage(MAX30105_SAMPLEAVG_8);
+        else if (sampleAverage == 16) setFIFOAverage(MAX30105_SAMPLEAVG_16);
+        else if (sampleAverage == 32) setFIFOAverage(MAX30105_SAMPLEAVG_32);
+        else setFIFOAverage(MAX30105_SAMPLEAVG_4);
+
+        //setFIFOAlmostFull(2); //Set to 30 samples to trigger an 'Almost Full' interrupt
+        enableFIFORollover(); //Allow FIFO to wrap/roll over
+
+        if (ledMode == 2) setLEDMode(MAX30105_MODE_REDIRONLY); //Red and IR
+        else setLEDMode(MAX30105_MODE_REDONLY); //Red only
+        activeDiodes = ledMode; //Used to control how many uint8_ts to read from FIFO buffer
+
+        if(adcRange < 4096) setADCRange(MAX30105_ADCRANGE_2048); //7.81pA per LSB
+        else if(adcRange < 8192) setADCRange(MAX30105_ADCRANGE_4096); //15.63pA per LSB
+        else if(adcRange < 16384) setADCRange(MAX30105_ADCRANGE_8192); //31.25pA per LSB
+        else if(adcRange == 16384) setADCRange(MAX30105_ADCRANGE_16384); //62.5pA per LSB
+        else setADCRange(MAX30105_ADCRANGE_2048);
+
+        if (sampleRate < 100) setSampleRate(MAX30105_SAMPLERATE_50); //Take 50 samples per second
+        else if (sampleRate < 200) setSampleRate(MAX30105_SAMPLERATE_100);
+        else if (sampleRate < 400) setSampleRate(MAX30105_SAMPLERATE_200);
+        else if (sampleRate < 800) setSampleRate(MAX30105_SAMPLERATE_400);
+        else if (sampleRate < 1000) setSampleRate(MAX30105_SAMPLERATE_800);
+        else if (sampleRate < 1600) setSampleRate(MAX30105_SAMPLERATE_1000);
+        else if (sampleRate < 3200) setSampleRate(MAX30105_SAMPLERATE_1600);
+        else if (sampleRate == 3200) setSampleRate(MAX30105_SAMPLERATE_3200);
+        else setSampleRate(MAX30105_SAMPLERATE_50);
+
+        if (pulseWidth < 118) setPulseWidth(MAX30105_PULSEWIDTH_69); //Page 26, Gets us 15 bit resolution
+        else if (pulseWidth < 215) setPulseWidth(MAX30105_PULSEWIDTH_118); //16 bit resolution
+        else if (pulseWidth < 411) setPulseWidth(MAX30105_PULSEWIDTH_215); //17 bit resolution
+        else if (pulseWidth == 411) setPulseWidth(MAX30105_PULSEWIDTH_411); //18 bit resolution
+        else setPulseWidth(MAX30105_PULSEWIDTH_69);
+
+        //LED Pulse Amplitude Configuration
+        //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+        //Default is 0x1F which gets us 6.4mA
+        //powerLevel = 0x02, 0.4mA - Presence detection of ~4 inch
+        //powerLevel = 0x1F, 6.4mA - Presence detection of ~8 inch
+        //powerLevel = 0x7F, 25.4mA - Presence detection of ~8 inch
+        //powerLevel = 0xFF, 50.0mA - Presence detection of ~12 inch
+
+        setPulseAmplitudeRed(powerLevel);
+        setPulseAmplitudeIR(powerLevel);
+
+        enableSlot(1, SLOT_RED_LED);
+        if (ledMode > 1) enableSlot(2, SLOT_IR_LED);
+        //enableSlot(1, SLOT_RED_PILOT);
+        //enableSlot(2, SLOT_IR_PILOT);
+        //enableSlot(3, SLOT_GREEN_PILOT);
+
+        clearFIFO(); //Reset the FIFO before we begin checking the sensor
+    }
+
 	/**
 	* Initializes the gator:particle sensor, must be called on power up
 	*/	
@@ -90,6 +244,7 @@ namespace CO2 {
 	//% blockId="gatorParticle_begin" 
 	//% block="initialize gator:Particle sensor"
 	export function begin(){
+		particle_setup(0x1F, 4, 2, 400, 411, 4096);
 		return
 	}
 		
