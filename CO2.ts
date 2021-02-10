b5172ae416ef1fd1cdee0346b1b6f0b09f64abf4
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2021-02-10 15:15:25 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -399,7 +399,7 @@ const FIRCoeffs = [172, 321, 579, 927, 1360, 1858, 2390, 2916, 3391, 3768, 4012,
 	*/
 	//% weight=29 
 	//% blockId="gatorParticle_color" 
-	//% block="get %LEDToRead value"
+	//% block="get Red:1 Infrared:2 %LEDToRead value"
 	export function color(type: LEDToRead): number{
 		return 0
 	}
@@ -409,7 +409,7 @@ const FIRCoeffs = [172, 321, 579, 927, 1360, 1858, 2390, 2916, 3391, 3768, 4012,
 	*/	
 	//% weight=28
 	//% blockId="gatorParticle_setReadMode"
-	//% block="set LED mode to read %LEDMode"
+	//% block="set LED mode to read Red:2 Red&Infrared:3 %LEDMode"
 	//% shim=gatorParticle::setReadMode
 	export function setReadMode(mode: LEDMode)
 	{
@@ -421,7 +421,7 @@ const FIRCoeffs = [172, 321, 579, 927, 1360, 1858, 2390, 2916, 3391, 3768, 4012,
 	*/	
 	//% weight=27
 	//% blockId="gatorParticle_setAmplitude"
-	//% block="change strength of %LEDToRead | to %myBrightness"
+	//% block="change strength of Red:1 Infrared:2 %LEDToRead | to %myBrightness"
 	//% advanced=true
 	export function setAmplitude(led: LEDToRead, myBrightness: number)
 	{
@@ -433,7 +433,7 @@ const FIRCoeffs = [172, 321, 579, 927, 1360, 1858, 2390, 2916, 3391, 3768, 4012,
 	*/
 	//% weight=26
 	//% blockId="gatorParticle_heartbeat"
-	//% block="detect heartbeat in %HeartbeatType"
+	//% block="detect heartbeat in BPM:0 AVG:1 %HeartbeatType"
 	export function heartbeat(type: HeartbeatType): number
 	{
 		return 0
