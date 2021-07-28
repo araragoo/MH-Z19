f93de57cb98bfcd4bdd5a6fe08843851ca3e71ed
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2021-07-28 16:48:08 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -9,6 +9,49 @@ namespace CO2 {
     let buf0 = control.createBuffer(9)
 
     //% subcategory="CO2"
+    //% blockId=setOffset
+    //% block="Set CO2 value as 400ppm"
+    export function CO2SetOffset () {
+
+        basic.showString("Wait 20min for setting 400ppm! ")
+        for (let m = 0; m < 20; m++) {
+            for (let index = 0; index < 12; index++) {
+                basic.showString("" + convertToText(21 - m) + "m")
+            }
+        }
+        for (let s = 0; s < 12; s++) {
+            basic.showString("" + convertToText(60 - s*5) + "s")
+        }
+
+        serial.writeBuffer(buf0)
+        basic.pause(100)
+        basic.showString("Done!")
+    }
+
+    //% subcategory="CO2"
+    //% blockId=measureCO2
+    //% block="CO2[ppm]"
+    export function CO2Value(): number {
+
+        serial.writeBuffer(buf)
+        basic.pause(100)
+
+        buffer = serial.readBuffer(9)
+        if (buffer.getNumber(NumberFormat.UInt8LE, 0) == 255 && buffer.getNumber(NumberFormat.UInt8LE, 1) == 134) {
+//            let sum = 0
+//            for (let index = 0; index <= 7; index++) {
+//                sum = sum + buffer.getNumber(NumberFormat.UInt8LE, index)
+//            }
+//            sum = sum % 256
+//            sum = 255 - sum
+//            if (sum == buffer.getNumber(NumberFormat.UInt8LE, 8)) {
+                CO2data = buffer.getNumber(NumberFormat.UInt8LE, 2) * 256 + buffer.getNumber(NumberFormat.UInt8LE, 3)
+//            }
+        }
+        return CO2data
+    }
+
+    //% subcategory="CO2"
     //% blockId=initalCO2
     //% block="Init CO2"
     export function CO2Init () {
@@ -38,48 +81,6 @@ namespace CO2 {
         buf0.setNumber(NumberFormat.UInt8LE, 8, 120)
     }
 
-    //% subcategory="CO2"
-    //% blockId=measureCO2
-    //% block="CO2[ppm]"
-    export function CO2Value(): number {
-
-        serial.writeBuffer(buf)
-        basic.pause(100)
-
-        buffer = serial.readBuffer(9)
-        if (buffer.getNumber(NumberFormat.UInt8LE, 0) == 255 && buffer.getNumber(NumberFormat.UInt8LE, 1) == 134) {
-//            let sum = 0
-//            for (let index = 0; index <= 7; index++) {
-//                sum = sum + buffer.getNumber(NumberFormat.UInt8LE, index)
-//            }
-//            sum = sum % 256
-//            sum = 255 - sum
-//            if (sum == buffer.getNumber(NumberFormat.UInt8LE, 8)) {
-                CO2data = buffer.getNumber(NumberFormat.UInt8LE, 2) * 256 + buffer.getNumber(NumberFormat.UInt8LE, 3)
-//            }
-        }
-        return CO2data
-    }
-
-    //% subcategory="CO2"
-    //% blockId=setOffset
-    //% block="Set CO2 value as 400ppm"
-    export function CO2SetOffset () {
-
-        basic.showString("Wait 20min for setting 400ppm! ")
-        for (let m = 0; m < 20; m++) {
-            for (let index = 0; index < 12; index++) {
-                basic.showString("" + convertToText(21 - m) + "m")
-            }
-        }
-        for (let s = 0; s < 12; s++) {
-            basic.showString("" + convertToText(60 - s*5) + "s")
-        }
-
-        serial.writeBuffer(buf0)
-        basic.pause(100)
-        basic.showString("Done!")
-    }
 
     const MAX30105_ADDRESS = 0x57; //7-bit I2C Address
     const I2C_BUFFER_LENGTH = 32;
