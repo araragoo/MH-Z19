416b22d9cd17c69ba0784dfcd41e697cc6a802a2
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2021-07-29 12:07:45 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -565,11 +565,11 @@ namespace CO2 {
             irValue = getIR();
          //   if (checkForBeat(irValue) == true) {
                 delta = control.millis() - lastBeat;
-                lastBeat = control.millis();
+                //lastBeat = control.millis();
           
                 beatsPerMinute = 60 * 1000 / delta;
 
-                beatsPerMinute = lastBeat;
+                beatsPerMinute = delta;
 
 //                if (beatsPerMinute < 255 && beatsPerMinute > 20) {
                     rates[rateSpot++] = beatsPerMinute; //Store this reading in the array
