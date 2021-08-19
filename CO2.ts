b2b082dca311feea1f04caadc0751fe2d03ab1b7
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2021-08-19 10:06:16 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -1173,7 +1173,7 @@ namespace CO2 {
         for(let i=0; i < num; i++) {
             sum += readTemp(MLX90614_TOBJ1);
         }
-        sum = sum * k_body / num;
+        sum = sum / num;// * k_body;
         return sum;
     }
 }
