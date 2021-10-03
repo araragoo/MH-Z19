7b30773bbd3ed09eba89cfcc102d1d27a7b17da4
araragoo <50019143+araragoo@users.noreply.github.com>
2021-10-03 16:16:14 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -415,7 +415,7 @@ let HeartRate = 0;
         let readPointer = i2cread(MAX30100_I2C_ADDRESS, MAX30100_REG_FIFO_READ_POINTER)
         let writePointer = i2cread(MAX30100_I2C_ADDRESS, MAX30100_REG_FIFO_WRITE_POINTER);
         let toRead = (writePointer - readPointer) & (MAX30100_FIFO_DEPTH-1);
-//        basic.showNumber(toRead);
+basic.showNumber(toRead);
         if (toRead) {
             burstRead(MAX30100_REG_FIFO_DATA, 4 * toRead);
     
