ab857121a6449f32fc1141acd50dfabad0df4660
araragoo <50019143+araragoo@users.noreply.github.com>
2021-10-03 16:19:54 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -429,6 +429,7 @@ basic.showNumber(toRead);
                 }
                 sense_IR[sense_head]  = (readbuf[i*4] << 8) | readbuf[i*4 + 1];
                 sense_red[sense_head] = (readbuf[i*4 + 2] << 8) | readbuf[i*4 + 3];
+basic.showNumber(sense_IR[sense_head];
             }
         }
     }
