e2636bb731aae2d95d6c40779f7480959aa83420
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2021-10-04 18:09:56 +0900

    Update CO2.ts

--- CO2.ts
+++ CO2.ts
@@ -410,9 +410,11 @@ let HeartRate = 0;
 
     function readFifoData() {
         let writePointer = i2cread(MAX30100_I2C_ADDRESS, MAX30100_REG_FIFO_WRITE_POINTER);
+basic.showNumber(writePointer);
         let readPointer = i2cread(MAX30100_I2C_ADDRESS, MAX30100_REG_FIFO_READ_POINTER);
+basic.showNumber(readPointer);
         let toRead = (writePointer - readPointer) & (MAX30100_FIFO_DEPTH-1);
-basic.showNumber(toRead);
+
 toRead=1;
         if (toRead) {
             burstRead(MAX30100_REG_FIFO_DATA, 4 * toRead);
